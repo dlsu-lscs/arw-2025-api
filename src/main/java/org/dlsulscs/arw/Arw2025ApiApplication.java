c9b69768e7806901e82f451d80afaffd57931b2b
ejsadiarin <ejs@ejsadiarin.com>
2025-08-30 03:15:28 +0800

    init: java springboot for api

---
+++ src/main/java/org/dlsulscs/arw/Arw2025ApiApplication.java
@@ -0,0 +1,13 @@
+package org.dlsulscs.arw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arw2025ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Arw2025ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dlsulscs/arw/Arw2025ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dlsulscs.arw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arw2025ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
