249809774eb51c0397b6dc50b12265083be25682
ejsadiarin <ejs@ejsadiarin.com>
2025-09-01 23:21:17 +0800

    chore: move security configs

--- src/main/java/org/dlsulscs/arw/config/SecurityConfig.java
+++ src/main/java/org/dlsulscs/arw/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.dlsulscs.arw.configs;
+package org.dlsulscs.arw.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
